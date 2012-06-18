7dc67de29da31adac685091e25c0f6ff5fcf8f77
Leotsarev <leo@bastilia.ru>
2012-06-18 18:48:20 +0400

    Забил коды для анализа

--- Logic/src/alpha/chem/Chemistry.java
+++ Logic/src/alpha/chem/Chemistry.java
@@ -13,28 +13,31 @@ public class Chemistry {
 	public Chemistry()
 	{
 		data = new IChemObject[]{
-				new Gene("A", "_", "_"),
-				new Gene("B", "_", "_"),
-				new Gene("O", "_", "_"),
-				new Gene("P", "_", "_"),
-				new Gene("X", "_", "_"),
-				new Gene("R", "_", "_"),
-				new Gene("G", "_", "_"),
-				new Gene("D", "_", "_"),
-				new Gene("F", "_", "_"),
-				new Gene("H", "_", "_"),
-				new Gene("K", "_", "_"),
-				new Gene("E", "_", "_"),
-				new Gene("Y", "_", "_"),
-				new Gene("L", "_", "_"),
-				new Gene("M", "_", "_"),
-				new Gene("U", "_", "_"),
-				new Gene("N", "_", "_"),
-				new Gene("T", "_", "_"),
-				new Gene("S", "_", "_"),
-				new Gene("C", "_", "_"),
-				new Gene("I", "_", "_"),
-				//TODO q,z,w
+				new Gene("A", "CAGGATCTGTTGCACTGGCC", "CAGGATCGGTTGCACTGGCC", Gene.AA),
+				new Gene("B", "ACTGGTGGTCTTAGCTTTTG", "ACTGGTAGTCTTAGCTTTTG", Gene.AA),
+				new Gene("O", "AAGCTTTCGTAAGTCTTCAC", "AAGCTTTCGTAAGTCTTCAC", Gene.aa),
+				new Gene("P", "TATAGCCTTCACCGAGTCAG", "AATAGCCTTCACCGAGTCAG", Gene.aa),
+				new Gene("X", "TTGCACGCATGGGCAATAAT", "TTGCACGCATGGGCAATAAA", Gene.Aa),
+				new Gene("R", "TCACCGTGGCCAGTCCTAAC", "TCACCGTGGCCAGTCCTAAA", Gene.aa),
+				new Gene("G", "CACCGAGTTTGCACGGGTCA", "CACCGAGTTTGCACGGGTCC", Gene.AA),
+				new Gene("D", "CTTCACTAGCTTGCTTCCTC", "CTTCACTAGTTTGCTTCCTC", Gene.AA),
+				new Gene("F", "GGGATCTAAGACACCTATGG", "GGTATCTAAGACACCTATGG", Gene.AA),
+				new Gene("H", "CACCTACGTGGTTAATACTA", "CACCTACGTGGCTAATACTA", Gene.AA),
+				new Gene("K", "ACGGTCACTCGGACCGAGAC", "TCGGTCAATCGGAGCGAGGC", Gene.aa),
+				new Gene("E", "TCTAGCGGTAGCATGCGCTG", "TGTAGCGGTAGCATGCGCTG", Gene.aa),
+				new Gene("Y", "GTGAACCTTGACCTGCTACG", "GTGAACATTGACCTGCTACG", Gene.aa),
+				new Gene("L", "TACATAGCCTGGACTAGCCA", "TACATAGCCTGGAGTAGCCA", Gene.Aa),
+				new Gene("M", "CATAGACTGGTACGCTGTAA", "CATAGACTGGTACGCTGCAA", Gene.AA),
+				new Gene("U", "TGCTGCCGTAACACTACGAC", "TGATGCCGTAACACTACGAC", Gene.aa),
+				new Gene("N", "CAATACGTGACTTTGACCTA", "CAATACGTGACTTGGACCTA", Gene.Aa),
+				new Gene("T", "TGGTACCATCAGTCACCAAG", "TGGTACCATCAGTCACTAAG", Gene.aa),
+				new Gene("S", "GCATGCACGGTCTAGTCAGG", "GCATGCACGGTCTCGTCAGG", Gene.aa),
+				new Gene("C", "ATATGCCTGGGACCTTCATG", "ATATGCCTGGGACATTCATG", Gene.Aa),
+				new Gene("I", "TTGACCCACGGGCTTGAATC", "TTGACCCACGGGCTTGAATC", Gene.aa),
+				
+				new Gene("q", "ANALYSIS_FAILED_CALL_MASTER", "ANALYSIS_FAILED_CALL_MASTER", Gene.aa),
+				new Gene("z", "ANALYSIS_FAILED_CALL_MASTER", "ANALYSIS_FAILED_CALL_MASTER", Gene.aa),
+				new Gene("w", "ANALYSIS_FAILED_CALL_MASTER", "ANALYSIS_FAILED_CALL_MASTER", Gene.aa),
 				
 				//Nutriens
 				new Nutrien("Alpha"),
@@ -143,9 +146,9 @@ public class Chemistry {
 	public final class Gene implements IPersistentChemObject {
 		
 		private static final int GENE_MIN_VALUE = 0;
-		private static final int GENE_aa = 0;
-		private static final int GENE_Aa = 1;
-		private static final int GENE_AA = 2;
+		private static final int aa = 0;
+		private static final int Aa = 1;
+		private static final int AA = 2;
 		private static final int GENE_MAX_VALUE = 2;
 		private int geneValue = GENE_MIN_VALUE;
 		
@@ -153,11 +156,16 @@ public class Chemistry {
 		private final String dominantAnalysis;
 		private final String recessiveAnalysis;
 		
-		private Gene(String myChar, String dominantAnalysis, String recessiveAnalysis)
+		private Gene(String myChar, String dominantAnalysis, String recessiveAnalysis, int geneValue)
 		{
 			this.myChar = myChar.toLowerCase();
 			this.dominantAnalysis = dominantAnalysis;
 			this.recessiveAnalysis = recessiveAnalysis;
+		}
+		
+		private Gene (String myChar, String dominantAnalysis, String recessiveAnalysis)
+		{
+			this (myChar, dominantAnalysis, recessiveAnalysis, aa);
 		}
 
 		public boolean isPresent() {
@@ -165,11 +173,11 @@ public class Chemistry {
 		}
 
 		public boolean isNotPresent() {
-			return geneValue == GENE_aa;
+			return geneValue == aa;
 		}
 
 		public boolean isStrictlyPresent() {
-			return geneValue == GENE_AA;
+			return geneValue == AA;
 		}
 
 		public String getName() {
@@ -196,15 +204,15 @@ public class Chemistry {
 		}
 
 		public void setPresent() {
-			geneValue = GENE_AA;
+			geneValue = AA;
 		}
 
 		public void setNotPresent() {
-			geneValue = GENE_aa;	
+			geneValue = aa;	
 		}
 
 		public void setHalfPresent() {
-			geneValue = GENE_Aa;
+			geneValue = Aa;
 		}
 		
 		private void checkGeneValue(int geneValue) {
@@ -215,11 +223,11 @@ public class Chemistry {
 		public String toString()
 		{
 			switch (geneValue) {
-			case GENE_aa:	
+			case aa:	
 					return myChar + myChar;
-			case GENE_Aa:
+			case Aa:
 				return myChar.toUpperCase() + myChar;
-			case GENE_AA:
+			case AA:
 				return myChar.toUpperCase() + myChar.toUpperCase();
 			default:
 				Utils.assert_(false);
@@ -230,11 +238,11 @@ public class Chemistry {
 		
 		public final String getAnalyzeResult() {
 			switch (geneValue) {
-			case GENE_aa:	
+			case aa:	
 					return getRecessiveAnalysis() + getRecessiveAnalysis();
-			case GENE_Aa:
+			case Aa:
 				return getDominantAnalysis() + getRecessiveAnalysis();
-			case GENE_AA:
+			case AA:
 				return getDominantAnalysis() + getDominantAnalysis();
 			default:
 				Utils.assert_(false);
