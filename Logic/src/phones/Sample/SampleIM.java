9830092cac41aeac290cf5fdafe0089edb29ca5f
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2012-06-19 07:38:25 +0400

    improve sample model a little bit

--- Logic/src/phones/Sample/SampleIM.java
+++ Logic/src/phones/Sample/SampleIM.java
@@ -9,27 +9,57 @@ import phones.InteractionModel;
 public class SampleIM extends InteractionModel {
 
 	boolean state;
+	String command;
+	
 	public void reset() {
 		state = true;
+		command = null;
 	}
 
 	public Descriptor whatNext(int timePassed, Date currentTime) {
-		state = !state;
+		if ("123".equals(command)) {
+			command = null;
+			MenuDescriptor result = new MenuDescriptor();
+			result.timeout = 10;
+			result.menuHeader = "Choose one option:";
+			result.addItem("Apples","APPLE");
+			result.addItem("bananas", "BANANA");
+			result.timeoutCommand = "TIMEOUT";
+			result.alarm = ALARM_SINGLE;
+			return result;
+		}
+		
+		SleepDescriptor result = new SleepDescriptor();
+		result.timeout = 10;
+		if (command != null) {
+			result.status = command+" was entered!";
+			command = null;
+		} else {
+			String time = "fucking jme";//DateFormat.getTimeInstance().format(currentTime); 
+			result.status = "It's " + time + ".";
+		}
+		return result;
+		
+		/*state = !state;
 		if (state) {
 			SleepDescriptor result = new SleepDescriptor();
 			result.timeout = 10;
 			String time = "fucking jme";//DateFormat.getTimeInstance().format(currentTime); 
 			result.status = "It's " + time + ".";
+			if (command != null)
+				result.status = command+" was entered!";
+			command = null;
 			return result;
 		} else {
 			MenuDescriptor result = new MenuDescriptor();
-			result.timeout = 30;
+			result.timeout = 10;
 			result.menuHeader = "Choose one option:";
 			result.addItem("Apples","APPLE");
 			result.addItem("bananas", "BANANA");
 			result.timeoutCommand = "TIMEOUT";
+			command = null;
 			return result;
-		}
+		}*/
 	}
 
 	public int checkCommandWord(String commandWord) {
@@ -48,7 +78,7 @@ public class SampleIM extends InteractionModel {
 
 	public void assertCommandWord(String code) {
 		System.out.println("SampleIM: code "+code+" received.");
-		
+		command = code;
 	}
 
 	public void unserialize(ISerializer serializedData) {
