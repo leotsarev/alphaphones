f4cbbde10d00c8fde1ebeb16c65dc723aa65b0e7
Leotsarev <leo@bastilia.ru>
2012-06-20 16:30:58 +0400

    Добавил FoodDeficitGamma

--- Logic/src/alpha/food/deficit/FoodDeficitGamma.java
+++ Logic/src/alpha/food/deficit/FoodDeficitGamma.java
@@ -13,11 +13,11 @@ public class FoodDeficitGamma extends alpha.food.FoodDeficitBase {
 	protected String getHealMessage() {
 		switch (getStage()) {
 		case 1:
-			return "__";
+			return "Все еще чешется...";
 		case 2:
-			return "__";
+			return "А вот и чесотка, я скучал без нее... И как же бесят эти рожи! Зато ноги больше не беспокоят...";
 		case 3:
-			return "__";
+			return "Кажется, я могу встать на ноги. Но лучше не буду...";
 		}
 		Utils.assert_(false);
 		return null;
@@ -26,13 +26,13 @@ public class FoodDeficitGamma extends alpha.food.FoodDeficitBase {
 	protected String getProgressMessage() {
 		switch (getStage()) {
 		case 1:
-			return "__";
+			return "Отчаянно зудит кожа на запястьях, да и много где. Почешусь-ка от души под лопатками!";
 		case 2:
-			return "__";
+			return "Эта чесотка сведет меня с ума! И они постоянно хотят от меня всякой чуши. Наору на одного, может отстанут...";
 		case 3:
-			return "__";
+			return "Я очень слаб, и ноги меня плохо слушаются. Хорошо бы полежать минутку-другую...";
 		case 4:
-			return "__";
+			return "Ноги совсем перестали двигаться! Кто-то должен мне помочь, пока не поздно. Заставлю их, если потребуется!!";
 		}
 		Utils.assert_(false);
 		return null;
@@ -40,28 +40,28 @@ public class FoodDeficitGamma extends alpha.food.FoodDeficitBase {
 
 	protected void updateStatusForStage() {
 		switch (getStage()) {
+		case 1:
+			addStatusMessage(getName(), "Зуд на запястьях");
 		case 2:
-			
+			addStatusMessage(getName(), "Страшная чесотка. Окружающие раздражают");
 			break;
 		case 3:
-			
+			addStatusMessage(getName(), "Ноги не слушаются. Хочу лежать");
 			break;
 		case 4:
-			
+			addStatusMessage(getName(), "Ноги не слушаются, не могу стоять. Срочно нужна помощь");
 		default:
-			
 			break;
 		}
 	}
 
 	protected void cleanupStatus() {
-		// TODO Auto-generated method stub
-
+		removeStatusMessage(getName());
 	}
 
 	public String getName() {
 		// TODO Auto-generated method stub
-		return "____";
+		return "FoodDeficitGamma";
 	}
 
 }
