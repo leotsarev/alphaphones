3b6c1047068ae5da8f954dc72517b01d44009f85
Leotsarev <leo@bastilia.ru>
2012-06-21 14:33:54 +0400

    Вынесена логика разбора и проверки кода в отдельный класс

---
+++ Logic/src/alpha/ThreeDigitCodeBase.java
@@ -0,0 +1,43 @@
+package alpha;
+
+import phones.IPrefixHandler;
+import phones.ProcessModelBase;
+import phones.Utils;
+
+public abstract class ThreeDigitCodeBase extends AlphaProcess implements IPrefixHandler {
+
+	private static final int SUFFIX_LENGTH = 6;
+
+	public ThreeDigitCodeBase(ProcessModelBase model) {
+		super(model);
+	}
+
+	public final void setSuffixValue(String suffix) {
+		Utils.assert_(isValidSuffix(suffix));
+		setIntArg("code", getCodeFromSuffix(suffix));
+	}
+
+	public final boolean isValidSuffix(String suffix) {
+		if (suffix.length() != SUFFIX_LENGTH)
+		{
+			return false;
+		}
+		int foodFlag = getCodeFromSuffix(suffix);
+		int check = Integer.valueOf(suffix.substring(3, 6)).intValue();
+		
+		return Utils.quickHash(foodFlag) == check;
+	}
+
+	private int getCodeFromSuffix(String suffix) {
+		return Integer.valueOf(suffix.substring(0, 3)).intValue();
+	}
+
+	public final boolean isStartOfSuffix(String suffix) {
+		return suffix.length() <= SUFFIX_LENGTH;
+	}
+
+	protected final int getCode() {
+		return getIntArg("code");
+	}
+
+}
--- Logic/src/alpha/food/FoodItem.java
+++ Logic/src/alpha/food/FoodItem.java
@@ -1,14 +1,12 @@
 package alpha.food;
 
+import alpha.ThreeDigitCodeBase;
 import alpha.chem.Chemistry.Nutrien;
-import phones.IPrefixHandler;
 import phones.InteractionModel.Descriptor;
 import phones.ProcessModelBase;
 import phones.Utils;
 
-public class FoodItem extends alpha.AlphaProcess implements IPrefixHandler {
-
-	private static final int SUFFIX_LENGTH = 6;
+public class FoodItem extends ThreeDigitCodeBase {
 
 	public FoodItem(ProcessModelBase model) {
 		super(model);
@@ -18,35 +16,11 @@ public class FoodItem extends alpha.AlphaProcess implements IPrefixHandler {
 		return "FoodItem";
 	}
 
-	public void setSuffixValue(String suffix) {
-		Utils.assert_(isValidSuffix(suffix));
-		setIntArg("food", 97 - getfoodPartFromSuffix(suffix));
-	}
-
-	public boolean isValidSuffix(String suffix) {
-		if (suffix.length() != SUFFIX_LENGTH)
-		{
-			return false;
-		}
-		int foodFlag = getfoodPartFromSuffix(suffix);
-		int check = Integer.valueOf(suffix.substring(3, 6)).intValue();
-		
-		return Utils.quickHash(foodFlag) == check;
-	}
-
-	private int getfoodPartFromSuffix(String suffix) {
-		return Integer.valueOf(suffix.substring(0, 3)).intValue();
-	}
-
-	public boolean isStartOfSuffix(String suffix) {
-		return suffix.length() <= SUFFIX_LENGTH;
-	}
-
 	public Descriptor handle() {
 		Nutrien[] nutriens = getAlphaModel().Chemistry.getNutrienArray();
 		Utils.assert_(nutriens.length == 6);
 		
-		int foodFlag = getIntArg("food");
+		int foodFlag = 97 - getCode();
 		for (int i =0; i<nutriens.length; i++)
 		{
 			int bitFlag = foodFlag & (1 << i);
